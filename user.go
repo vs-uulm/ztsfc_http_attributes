b8029bf0147e381217042d870b15795d7e807965
leobrada <leo.bradatsch@gmail.com>
2022-06-05 17:10:37 +0000

    added pw authentication fails

--- user.go
+++ user.go
@@ -4,6 +4,7 @@ package attributes
 // See: https://stackoverflow.com/questions/23695479/how-to-format-timestamp-in-outgoing-json
 type User struct {
     UserID string `json:"userID"`
+    FailedPWAuthentication int `json:"failedPWAuthentication"`
     UsualTimeBegin int `json:"usualTimeBegin"`
     UsualTimeEnd int `json:"usualTimeEnd"`
     UsualServices []string `json:"usualServices"`
@@ -12,15 +13,17 @@ type User struct {
 func NewEmptyUser() *User {
     newUser := new(User)
     newUser.UserID = ""
-    newUser.UsualTimeBegin = 8
-    newUser.UsualTimeEnd = 16
+    newUser.FailedPWAuthentication = 0
+    newUser.UsualTimeBegin = 0
+    newUser.UsualTimeEnd = 0
     newUser.UsualServices = make([]string, 0)
     return newUser
 }
 
-func NewUser(_userID string, _usualTimeBegin, _usualTimeEnd int, _usualServices []string) *User {
+func NewUser(_userID string, _failedPWAuthentication, _usualTimeBegin, _usualTimeEnd int, _usualServices []string) *User {
     newUser := new(User)
     newUser.UserID = _userID
+    newUser.FailedPWAuthentication = _failedPWAuthentication
     newUser.UsualTimeBegin = _usualTimeBegin
     newUser.UsualTimeEnd = _usualTimeEnd
     newUser.UsualServices = _usualServices
