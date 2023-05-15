04a2b980c4acb3ea9f1f26076c2fd4848af651d7
leobrada <leo.bradatsch@gmail.com>
2023-05-15 11:25:28 +0200

    rate limiter added to support UsualAccessRate

--- user.go
+++ user.go
@@ -1,18 +1,24 @@
 package attributes
 
+import (
+	"golang.org/x/time/rate"
+)
+
 // TODO: Implement JSON comptible time; Implement Marhsaler Interface for that.
 // See: https://stackoverflow.com/questions/23695479/how-to-format-timestamp-in-outgoing-json
 type User struct {
-	UserID                 string   `json:"userID" yaml:"user_id"`
-	FailedPWAuthentication int      `json:"failedPWAuthentication" yaml:"failed_pw_authentication"`
-	EnterprisePresence     bool     `json:"enterprisePresence" yaml:"enterprise_presence"`
-	UsualTimeBegin         int      `json:"usualTimeBegin" yaml:"usual_time_begin"`
-	UsualTimeEnd           int      `json:"usualTimeEnd" yaml:"usual_time_end"`
-	UsualAcessRate         int      `json:"usualAccessRate" yaml:"usual_access_rate"`
-	UsualServices          []string `json:"usualServices" yaml:"usual_services"`
-	AllowedServices        []string `json:"allowedServices" yaml:"allowed_services"`
-	UsualDevices           []string `json:"usualDevices" yaml:"usual_devices"`
-	TrustHistory           int      `json:"trustHistory" yaml:"trust_history"`
+	UserID                 string     `json:"userID" yaml:"user_id"`
+	FailedPWAuthentication int        `json:"failedPWAuthentication" yaml:"failed_pw_authentication"`
+	EnterprisePresence     bool       `json:"enterprisePresence" yaml:"enterprise_presence"`
+	UsualTimeBegin         int        `json:"usualTimeBegin" yaml:"usual_time_begin"`
+	UsualTimeEnd           int        `json:"usualTimeEnd" yaml:"usual_time_end"`
+	UsualAcessRate         rate.Limit `json:"usualAccessRate" yaml:"usual_access_rate"` // describes 'UsualAccessRate' requests per second
+	UsualServices          []string   `json:"usualServices" yaml:"usual_services"`
+	AllowedServices        []string   `json:"allowedServices" yaml:"allowed_services"`
+	UsualDevices           []string   `json:"usualDevices" yaml:"usual_devices"`
+	TrustHistory           int        `json:"trustHistory" yaml:"trust_history"`
+
+	UsualAccessRateLimiter *rate.Limiter
 }
 
 func NewEmptyUser() *User {
@@ -27,6 +33,7 @@ func NewEmptyUser() *User {
 	newUser.AllowedServices = make([]string, 0)
 	newUser.UsualDevices = make([]string, 0)
 	newUser.TrustHistory = 0
+	newUser.UsualAccessRateLimiter = rate.NewLimiter(newUser.UsualAcessRate, newUser.UsualAcessRate)
 	return newUser
 }
 
@@ -43,5 +50,6 @@ func NewUser(_userID string, _failedPWAuthentication int, _enterprisePresence bo
 	newUser.AllowedServices = _allowedServices
 	newUser.UsualDevices = _usualDevices
 	newUser.TrustHistory = _trustHistory
+	newUser.UsualAccessRateLimiter = rate.NewLimiter(newUser.UsualAcessRate, newUser.UsualAcessRate)
 	return newUser
 }
