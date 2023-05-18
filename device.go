96fabad26cf69ab0ae0a59b151a079e78a04bd37
leobrada <leo.bradatsch@gmail.com>
2023-05-18 10:25:21 +0200

    more device attributes added

--- device.go
+++ device.go
@@ -1,44 +1,57 @@
 package attributes
 
 import (
-    logger "github.com/vs-uulm/ztsfc_http_logger"
+	logger "github.com/vs-uulm/ztsfc_http_logger"
 )
 
 type Device struct {
-    DeviceID string `json:"deviceID" yaml:"device_id"`
-    CurrentIP string `json:"currentIP" yaml:"current_ip"`
-    Revoked bool `json:"revoked" yaml:"revoked"`
+	DeviceID           string   `json:"deviceID" yaml:"device_id"`
+	CurrentIP          string   `json:"currentIP" yaml:"current_ip"`
+	Revoked            bool     `json:"revoked" yaml:"revoked"`
+	EnterprisePresence bool     `json:"enterprisePresence" yaml:"enterprise_presence"`
+	UsualServices      []string `json:"usualServices" yaml:"usual_services"`
+	UsualUser          []string `json:"usualUser" yaml:"usual_user"`
+	TrustHistory       int      `json:"trustHistory" yaml:"trust_history"`
 }
 
 func NewEmptyDevice() *Device {
-    newDevice := new(Device)
-    newDevice.DeviceID = ""
-    newDevice.CurrentIP = ""
-    newDevice.Revoked = false
-    return newDevice
+	newDevice := new(Device)
+	newDevice.DeviceID = ""
+	newDevice.CurrentIP = ""
+	newDevice.Revoked = false
+	newDevice.EnterprisePresence = false
+	newDevice.UsualServices = make([]string, 0)
+	newDevice.UsualUser = make([]string, 0)
+	newDevice.TrustHistory = 0
+	return newDevice
 }
 
-func NewDevice(_deviceID string, _currentIP string, _revoked bool) (*Device, error) {
-    newDevice := new(Device)
-    newDevice.DeviceID = _deviceID
-    newDevice.CurrentIP = _currentIP
-    newDevice.Revoked = _revoked
-    return newDevice, nil
+func NewDevice(_deviceID string, _currentIP string, _revoked bool, _enterprisePresence bool, _usualServices, _usualUser []string,
+	_trustHistory int) (*Device, error) {
+	newDevice := new(Device)
+	newDevice.DeviceID = _deviceID
+	newDevice.CurrentIP = _currentIP
+	newDevice.Revoked = _revoked
+	newDevice.EnterprisePresence = _enterprisePresence
+	newDevice.UsualServices = _usualServices
+	newDevice.UsualUser = _usualUser
+	newDevice.TrustHistory = _trustHistory
+	return newDevice, nil
 }
 
 func PrintDevices(sysLogger *logger.Logger, devices map[string]*Device) {
-    for _, deviceObj := range devices {
-        sysLogger.Infof("%v\n", deviceObj)
-    }
+	for _, deviceObj := range devices {
+		sysLogger.Infof("%v\n", deviceObj)
+	}
 }
 
 func FindDeviceByIPInIDMap(sysLogger *logger.Logger, ip string, devices map[string]*Device) *Device {
-    for _, device := range devices {
-        if device.CurrentIP == ip {
-            sysLogger.Debugf("attributes: FindDeviceByIPInMap(): device '%s' has currently IP addr '%s'", device.DeviceID, device.CurrentIP)
-            return device
-        }
-    }
+	for _, device := range devices {
+		if device.CurrentIP == ip {
+			sysLogger.Debugf("attributes: FindDeviceByIPInMap(): device '%s' has currently IP addr '%s'", device.DeviceID, device.CurrentIP)
+			return device
+		}
+	}
 
-    return nil
+	return nil
 }
